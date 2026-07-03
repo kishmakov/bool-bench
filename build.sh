6b2c1cfc2d76f919a1a8c343ca46827c0aaeaa53
Kirill Shmakov <shmakir@yandex.ru>
2026-07-03 09:40:39 +0200

    Make -O2 default state.

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-cmake -S . -B build -DBUILD_SHARED_LIBS=ON
-cmake --build build
+cmake -S . -B build \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG" \
+    -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG"
+cmake --build build --config Release
